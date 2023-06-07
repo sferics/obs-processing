42e4085c0190c5e6c6177816cb70d0483b97eec1
Juri Hubrig <juri@mswr.de>
2023-06-08 01:26:24 +0200

    Bugfixes

--- ecdecode_wmo.py
+++ ecdecode_wmo.py
@@ -12,15 +12,16 @@ db  = connect("localhost", "obs", "obs4data", "obs" )   # Opening database conne
 cur = db.cursor()                                       # Creating cursor object to call SQL
 #read sql files to create db tables and execute the SQL statements
 read_file = lambda file_name : Path( file_name ).read_text()
-for table in ("station", "obs"): cur.execute( read_file( table + ".sql" ) )
+for table in ("station", "obs"):
+    cur.execute( read_file( table + ".sql" ) )
 
-#constants
 bufr_dir      = "bufr/"
 processed_dir = bufr_dir + "processed/"
 skip          = ["unexpandedDescriptors", "timeIncrement"]
 station_info  = [ _ for _ in read_file( "station_info.txt" )[:-1].splitlines() ]
 time_keys     = ['year', 'month', 'day', 'hour', 'minute', 'timePeriod', 'timeSignificance']
 null_vals     = (2147483647, -1e+100, None, "None", "null", "NULL", "MISSING", "XXXX", {}, "", [], ())
+skip_obs      = False
 
 clear      = lambda keyname : re.sub( r"#[0-9]+#", '', keyname )
 number     = lambda keyname : int( re.sub( r"#[A-Za-z0-9]+", "", keyname[1:]) )
@@ -52,10 +53,10 @@ def sql_insert(table, params, ignore = False, update = None, skip_update = () ):
     return sql
 
 def known_stations():
-    cur.execute("SELECT DISTINCT `stationID` FROM `station`")
+    cur.execute("SELECT DISTINCT `stID` FROM `station`")
     data = cur.fetchall()
-    if data:    return (i[0] for i in data)
-    else:       return ()
+    if data: return (i[0] for i in data)
+    else:    return ()
 
 
 for FILE in glob( bufr_dir + "*.bin" ): #get list of files in bufr_dir
@@ -68,12 +69,12 @@ for FILE in glob( bufr_dir + "*.bin" ): #get list of files in bufr_dir
         except: continue
         
         keys, obs_keys, nums = [], [], []
-
+        
         while ec.codes_bufr_keys_iterator_next(iterid):
             
             keyname = ec.codes_bufr_keys_iterator_get_name(iterid)
             clear_key = clear(keyname)
-
+            
             if "#" in keyname and clear_key not in skip:
                 num = number(keyname)
                 if num not in nums: nums.append(num)
@@ -81,40 +82,41 @@ for FILE in glob( bufr_dir + "*.bin" ): #get list of files in bufr_dir
                     keys.append(clear_key)
                     if clear_key not in station_info:
                         obs_keys.append(clear_key)
-
+        
         for num in sorted(nums):
             obs, meta = {}, {}
             for si in station_info:
                 try:    meta[si] = get_bufr( bufr, num, si )
                 except: meta[si] = None
-
-            if meta["shortStationName"]: stationID = str(meta["shortStationName"])
+            
+            if meta["shortStationName"]: meta["stID"] = str(meta["shortStationName"])
             else:
                 if meta["stationNumber"] not in null_vals and meta["blockNumber"] not in null_vals:
-                    stationID = str(meta["stationNumber"] + meta["blockNumber"] * 1000).rjust(5,"0")
+                    meta["stID"] = str(meta["stationNumber"] + meta["blockNumber"]*1000).rjust(5, "0")
                 else: continue
-            meta["stationID"], obs["stationID"] = stationID, stationID
             
-            if ( stationID not in known_stations() ) and ( len(meta["stationOrSiteName"]) > 1 ):
+            if (meta["stID"] not in known_stations()) and (len(meta["stationOrSiteName"]) > 1):
                 print("Adding", meta["stationOrSiteName"], "to database...")
-                sql = sql_insert( "station", meta, ignore=True )
-                try: cur.execute( sql ); db.commit()
-                except: continue
-
+                cur.execute( sql_insert( "station", meta, ignore=True ) ); db.commit()
+            
             for key in obs_keys:
-                try: cur.execute( f"ALTER TABLE obs ADD COLUMN {key} VARCHAR(255)" ); db.commit()
+                try:    cur.execute(f"ALTER TABLE obs ADD COLUMN {key} VARCHAR(255)"); db.commit()
                 except: pass
                 #max length of mysql identifier is 64! TODO: write conversion dictionary
-                try: obs[key] = get_bufr( bufr, num, key[:64] )
+                try:    obs[key] = get_bufr( bufr, num, key[:64] )
                 except: continue
-
-            #insert obsdata to db; on duplicate key update only obs values; no stationID or time_keys
-            sql = sql_insert( "obs", obs, update = True, skip_update = time_keys + ["stationID"] )
-            try: cur.execute( sql )
-            except: continue
-    
+           
+            obs["stID"] = meta["stID"]
+            #we need correct date/time information, otherwise skip this obs!
+            for tk in time_keys[:5]:
+                if tk not in obs or obs[tk] in null_vals:
+                    skip_obs = True; break
+            if skip_obs: continue
+            #insert obsdata to db; on duplicate key update only obs values; no stID or time_keys
+            sql = sql_insert( "obs", obs, update = True, skip_update = time_keys + ["stID"] )
+            cur.execute( sql )
+            
     move( FILE, processed_dir + FILE.replace(bufr_dir, "") )    #move FILE to the "processed" folder
     ec.codes_release(bufr)                                      #release file to free memory
-
-db.commit()
-cur.close(); db.close()
+    
+db.commit(); cur.close(); db.close()
--- obs.sql
+++ obs.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `obs` (
   `obsID` int NOT NULL AUTO_INCREMENT,
-  `stationID` int NOT NULL,
+  `stID` varchar(255) NOT NULL,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `year` int NOT NULL,
   `month` int NOT NULL,
@@ -10,12 +10,12 @@ CREATE TABLE IF NOT EXISTS `obs` (
   `timeSignificance` int DEFAULT NULL,
   `timePeriod` int DEFAULT NULL,
   PRIMARY KEY (`obsID`),
-  UNIQUE KEY `unique_obs` (`obsID`,`stationID`,`year`,`month`,`day`,`hour`,`minute`,`timeSignificance`,`timePeriod`) USING BTREE,
+  UNIQUE KEY `unique_obs` (`obsID`,`stID`,`year`,`month`,`day`,`hour`,`minute`,`timeSignificance`,`timePeriod`) USING BTREE,
   KEY `year` (`year`),
   KEY `month` (`month`),
   KEY `day` (`day`),
   KEY `hour` (`hour`) USING BTREE,
   KEY `minute` (`minute`),
-  KEY `stationID` (`stationID`)
+  KEY `stID` (`stID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 COMMIT;
