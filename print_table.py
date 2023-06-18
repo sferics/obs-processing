08628705cfd6c0ca752f5ad2872a73e89938e831
Juri Hubrig <juri@mswr.de>
2023-06-18 19:50:25 +0200

    print_table.py now takes a 3rd command line argument WHERE

--- print_table.py
+++ print_table.py
@@ -8,9 +8,13 @@ if len(argv) == 3:
     what = argv[2]
 else: what = "*"
 
+if len(argv) == 4:
+    where = f" WHERE {argv[3]}"
+else: where = ""
+
 from sqlite3 import connect
 db=connect("obs.db")
 cur=db.cursor()
-cur.execute(f"SELECT {what} FROM {table}")
+cur.execute(f"SELECT {what} FROM {table}{where}")
 data=cur.fetchall()
 print(data)
