0fec7b3f2a1eb21b620b68cd7e3fa16f90ee386c
Juri Hubrig <juri@mswr.de>
2023-06-21 11:40:51 +0200

    Added getSMHI.py for API download of Swedish OpenData

---
+++ getSMHI.py
@@ -0,0 +1,16 @@
+import requests
+
+headers = {'Accept': 'application/json'}
+
+#API documentation:
+#https://opendata.smhi.se/apidocs/metobs/stationSet.html
+
+#url = "https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/1/station/159880/period/latest-months.json"
+url = 'https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/%d/station-set/all/period/latest-hour/data.json'
+
+for i in range(1,41):
+    print(i)
+    print(url % i)
+    r = requests.get( url %i, headers=headers )
+    try: print(f"Response: {r.json()}")
+    except: continue
