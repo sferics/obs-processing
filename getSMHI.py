a2a5b45b76b7b03a6a610d44b524a1a3113a81b1
Juri Hubrig <juri@mswr.de>
2023-06-27 01:17:41 +0200

    TODO getSMHI.py

--- getSMHI.py
+++ getSMHI.py
@@ -4,6 +4,8 @@ headers = {'Accept': 'application/json'}
 
 #API documentation:
 #https://opendata.smhi.se/apidocs/metobs/stationSet.html
+#TODO PARAMETERS dict
+#https://opendata.smhi.se/apidocs/metobs/parameter.html
 
 #url = "https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/1/station/159880/period/latest-months.json"
 url = 'https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/%d/station-set/all/period/latest-hour/data.json'
