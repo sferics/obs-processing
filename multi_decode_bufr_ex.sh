6d9f300d437864b1f7a531496f2544473a957da3
Juri Hubrig <juri@mswr.de>
2024-01-29 18:51:04 +0000

    Multi decoder bufr decode_bufr_ex.py script

---
+++ multi_decode_bufr_ex.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# start poor-man's multi processing for the decode_bufr script.py by just starting it N times (default is 15)
+
+if [ $# -ge 1 ]
+then
+    N=$1
+else
+    N=15
+fi
+
+python -m compileall
+source export_bufr_tables.sh
+
+echo "Starting decode_bufr_se.py $N times..."
+
+for (( c=1; c<=$N; c++ )); do
+	python decode_bufr_ex.py test -r & sleep 2
+done
