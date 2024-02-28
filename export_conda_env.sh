4d7e6936f7b00903cb3a2fc914cfdeeff2b0a182
Juri Hubrig <juri@mswr.de>
2024-02-28 13:21:56 +0000

    Updated export_conda_env and install scripts

--- export_conda_env.sh
+++ export_conda_env.sh
@@ -1,1 +1,2 @@
+# https://python-bloggers.com/2021/09/creating-and-replicating-an-anaconda-environment-from-a-yaml-file/
 conda env export > config/obs_env.yml
--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 ln -s conclude_obs.py finalize_obs.py
+git clone https://github.com/sferics/plbufr/
 conda env create -f config/obs_env.yml
+cd plbufr && python setup.py install
