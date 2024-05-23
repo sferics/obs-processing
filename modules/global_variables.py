cd08fed37ea24b103f5ae69ef9d3a436c1874ce9
Juri Hubrig <juri@mswr.de>
2024-05-23 20:18:24 +0000

    Removed unused global_classes.py and redundant lines from global_variables.py

--- modules/global_variables.py
+++ modules/global_variables.py
@@ -3,32 +3,11 @@
 import datetime
 import numpy as np
 
-# date related global variables
-start_date = datetime.datetime(2021,1,1)
-end_date   = datetime.datetime(2023,1,1)
-
-# release
-release='2306'
-
-# seasons
-season_threshold=300
-seasons = [('01-12', '03-01'), # added a day to avoid the variable ending of february
-           ('02-03', '05-31'),
-           ('06-01', '08-31'),
-           ('09-01', '11-30')]
-
-# logger level
+# default logger level
 logger_level='WARNING' # options are: DEBUG, INFO, WARNING, ERROR, CRITICAL, NOTSET
 
 # all available log levels
 log_levels = { "CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG", "NOTSET" }
-
-# regorder_rules
-# {iteration_phase : accepted_predictor_groups}
-regorder={1:(1,2,3),
-          2:(2,5),
-          3:(1,2,3,4,5)}
-
 
 # generic variables that signify empty or non-numeric values
 null_vars = { np.nan, np.inf, np.NINF, "", None, }
